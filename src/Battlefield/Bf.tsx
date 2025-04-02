3b766fb7a443de18a36f0de8bd871841bca5429a
JL-Huawei_Graznov <graznov@me.com>
2025-04-02 09:47:07 +0300

    02.04.25 Mission

--- src/Battlefield/Bf.tsx
+++ src/Battlefield/Bf.tsx
@@ -1,8 +1,36 @@
 import classNames from 'classnames/bind';
 import styles from './bf.module.css';
-import Mission from "./Mission/Mission.tsx";
+import Mission, {props} from "./Mission/Mission.tsx";
 
 const cx = classNames.bind(styles);
+
+const temp:props =   {
+    "title": "Вопрос 1",
+    "ticket_number": "Билет 1",
+    "ticket_category": "A,B",
+    "image": "./images/A_B/1ca7f207d1d6cb37e12f5835c42a041c.jpg",
+    "question": "В каком случае водитель совершит вынужденную остановку?",
+    "answers": [
+        {
+            "answer_text": "Остановившись непосредственно перед пешеходным переходом, чтобы уступить дорогу пешеходу",
+            "is_correct": false
+        },
+        {
+            "answer_text": "Остановившись на проезжей части из-за технической неисправности транспортного средства",
+            "is_correct": true
+        },
+        {
+            "answer_text": "В обоих перечисленных случаях",
+            "is_correct": false
+        }
+    ],
+    "correct_answer": "Правильный ответ: 2",
+    "answer_tip": "«Вынужденная остановка» - прекращение движения транспортного средства, связанное с его технической неисправностью, опасностью, создаваемой перевозимым грузом, состоянием водителя (пассажира) или появления препятствия на дороге.(Пункт 1.2 ПДД, термин «Вынужденная остановка»)",
+    "topic": [
+        "Общие положения"
+    ],
+    "id": "ffd0c95b28a89bd4faff45a8874c4fb3"
+}
 
 function Bf(){
 
@@ -21,16 +49,27 @@ function Bf(){
 
             <main>
                 <div className={cx('content')}>
-                    <Mission/>
+                    <Mission
+                        title={temp.title}
+                        ticket_category={temp.ticket_category}
+                        ticket_number={temp.ticket_number}
+                        image={temp.image}
+                        question={temp.question}
+                        answers={temp.answers}
+                        correct_answer={temp.correct_answer}
+                        answer_tip={temp.answer_tip}
+                        topic={temp.topic}
+                        id={temp.id}
+                    />
 
                 </div>
             </main>
 
-            <footer>
-                <div className={cx('content')}>
+            {/*<footer>*/}
+            {/*    <div className={cx('content')}>*/}
 
-                </div>
-            </footer>
+            {/*    </div>*/}
+            {/*</footer>*/}
 
 
 
--- src/Battlefield/Mission/Mission.tsx
+++ src/Battlefield/Mission/Mission.tsx
@@ -3,11 +3,48 @@ import styles from './mission.module.css';
 
 const cx = classNames.bind(styles);
 
-function Mission(){
+export interface props {
+        image: string;
+        question: string;
+        ticket_category: string;
+        answers: ({ answer_text: string; is_correct: boolean } | { answer_text: string; is_correct: boolean } | {
+            answer_text: string;
+            is_correct: boolean
+        })[];
+        correct_answer: string;
+        topic: string[];
+        id: string;
+        title: string;
+        ticket_number: string;
+        answer_tip: string
+    }
 
-    return(
+
+function Mission({title, answers, answer_tip, correct_answer, id, image, question, ticket_category, ticket_number, topic}: props) {
+
+    const pathToImg = image.substr(1)
+
+        // ./images/A_B/eca08a0e2b5ffcd12bdd8ffee34afcc3.jpg
+
+    return (
         <div className={cx('mission')}>
-            Mission
+            <div className={cx('mission_title')}>{title} {ticket_number}</div>
+            {/*<div></div>*/}
+            {/*<div>{ticket_category}</div>*/}
+
+            <div className={cx('mission_img')}>
+                <img
+                    src={`pdd_russia${pathToImg}`}
+                    alt={`${pathToImg}`}/>
+            </div>
+
+
+            <div className={cx('mission_question')}>{question}</div>
+            <div>{JSON.stringify(answers)}</div>
+            <div>{correct_answer}</div>
+            <div>{answer_tip}</div>
+            <div>{JSON.stringify(topic)}</div>
+            <div>{id}</div>
         </div>
     )
 }
